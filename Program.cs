32c30bc0e986440c7ee522b72c08e3cca0bf14ed
Anastasia203 <70905660+Anastasia203@users.noreply.github.com>
2020-09-08 20:12:48 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace lab0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Имя пользователя:");
+            name = Console.ReadLine();
+            Console.WriteLine("Привет, {0}", name);
+        }
+    }
+}
