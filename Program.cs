1d3783164738a1c9dfc773b8013b0c65e3fce42f
Anastasia203 <70905660+Anastasia203@users.noreply.github.com>
2020-09-29 20:12:18 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,85 @@
 ﻿using System;
-namespace lab0
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace lab1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void NonNegativeInteger(ref int width, ref int height)//проверка высоты и ширины
         {
-            string name;
-            Console.WriteLine("Имя пользователя:");
-            name = Console.ReadLine();
-            Console.WriteLine("Привет, {0}", name);
+            while (height <= 0 || width <= 0)
+            {
+                Console.WriteLine("Ширина и высота должны быть больше 0");
+                Console.WriteLine("Ширина:"); width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Высота:"); height = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static void Main(string[] args)   
+        {
+            int width, height, x, y;
+            bool s = true;     
+            Console.WriteLine("Введите координаты:");
+            Console.Write("x="); x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y="); y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ширина:"); width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Высота:"); height = Convert.ToInt32(Console.ReadLine());
+            NonNegativeInteger(ref width, ref height);
+            Console.WriteLine("ширина {0};высота {1}",width, height);
+            Rectangles rec = new Rectangles(width, height, x, y);
+            while (s)
+            {
+                Console.WriteLine("Выберите действие\n" +
+                    "0. Завершить программу\n1. Переместить прямоугольник\n2. Изменить размер\n" +
+                "3. Построить прямоугольник, содержащий два заданных прямоугольника\n" +
+                "4. Пересечение с другим прямоугольником");
+                int c = Convert.ToInt32(Console.ReadLine());
+                switch (c)
+                {
+                    case 0:
+                        s = false;
+                        break;
+                    case 1: //перемещение
+                        Console.WriteLine("Введите координаты:");
+                        Console.Write("x="); x = Convert.ToInt32(Console.ReadLine()); 
+                        Console.Write("y="); y = Convert.ToInt32(Console.ReadLine());
+                        rec.Moving(x, y);
+                        rec.GetRec();
+                        break;
+                    case 2: //размер
+                        Console.WriteLine("Введите новую ширину:"); width = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите новую высота:"); height = Convert.ToInt32(Console.ReadLine());
+                        NonNegativeInteger(ref width, ref height);
+                        rec.Resize(width, height);
+                        rec.GetRec();
+                        break;
+                    case 3://2 в 1
+                        Console.WriteLine("Введите координаты второго прямоугольника:");
+                        Console.Write("x="); x = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("y="); y = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ширина:");  width = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Высота:");  height = Convert.ToInt32(Console.ReadLine());
+                        Rectangles rec2 = new Rectangles(width, height, x, y);
+                        rec.MinRec(rec.width, rec.height, rec.x, rec.y, rec2.width, rec2.height, rec2.x, rec2.y);
+                        break;
+                    case 4: //пересечение 
+                        Console.WriteLine("Введите координаты второго прямоугольника:");
+                        Console.Write("x="); x = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("y="); y = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ширина:"); width = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Высота:"); height = Convert.ToInt32(Console.ReadLine());
+                        Rectangles rec3 = new Rectangles(width, height, x, y);
+                        rec3.Intersection(rec.width, rec.height, rec.x, rec.y, rec3.width, rec3.height, rec3.x, rec3.y);
+                        rec3.GetRec();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
---
+++ Rectangles.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Rectangles
+    {
+        public int width, height, x, y;
+        public Rectangles(int width, int height, int x, int y)
+        {
+            this.width = width; this.height = height; this.x = x; this.y = y;
+        }
+        public void Moving(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+        public void Resize(int width, int height)
+        {
+            this.width = width; this.height = height;
+        }
+        public void MinRec(int width, int height, int x, int y, int width1, int height1, int x1, int y1)
+        {
+            int ymax = Math.Max(y, y1);
+            int ymin = Math.Min(y - height, y1 - height1);
+            int xmax = Math.Max(x + width1, x1 + width1);
+            int xmin = Math.Max(x, x1);
+            Console.WriteLine("Координаты полученного прямоугольника: ({0},{1}), ({2},{3}), " +
+                            "({4},{5}), ({6},{7})", xmin, ymax, xmax, ymax, xmax, ymin, xmin, ymin);
+        }
+        public void Intersection(int width, int height, int x, int y, int width1, int height1, int x1, int y1)
+        {
+            int w = 0, xb = 0, ya = 0, yb = 0, h = 0;
+            int yma = Math.Max(y, y1);
+            int ym = Math.Min(y - height, y1 - height1);
+            int xma = Math.Max(x + width, x1 + width1); //крайняя правая точка
+            int xm = Math.Max(x, x1); //крайняя левая точка
+            if (xma < xm)
+            {
+                Console.WriteLine("Прмоугольники не пересекаются");
+            }
+            else
+            {
+                if (x + width < x1 + width1)
+                {
+                    int xa = x + width; xb = x1;
+                    w = xa - xb;
+                }
+                else if (x + width > x1 + width1)
+                {
+                    int xa = x1 + width1; xb = x;
+                    w = xa - xb;
+                }
+                if (y <= y1 && (y - height) >= (y1 - height1))
+                {
+                    ya = y; yb = y - height;
+                    h = ya - yb;
+                }
+                else if (y > y1 && (y - height) >= (y1 - height1))
+                {
+                    ya = y1; yb = y1 - height1;
+                    h = ya - yb;
+                }
+            }
+           
+        }
+        public void GetRec()
+        {
+            Console.WriteLine("Координаты полученного прямоугольника: ({0},{1}), ({2},{3}), " +
+               "({4},{5}), ({6},{7})", x, y, x + width, y, x + width, y - height, x, y - height);
+        } 
+    }
+}
