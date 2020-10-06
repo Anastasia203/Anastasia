a760ee560ca6b85ff93d14c769bcfc70cebd24c6
Anastasia203 <70905660+Anastasia203@users.noreply.github.com>
2020-10-06 19:35:57 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +11,26 @@ namespace lab1
 {
     class Program
     {
-        static void NonNegativeInteger(ref int width, ref int height)//проверка высоты и ширины
+        static void NonNegativeNumber(ref double width, ref double height)//проверка высоты и ширины
         {
             while (height <= 0 || width <= 0)
             {
                 Console.WriteLine("Ширина и высота должны быть больше 0");
-                Console.WriteLine("Ширина:"); width = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Высота:"); height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ширина:"); width = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Высота:"); height = Convert.ToDouble(Console.ReadLine());
             }
         }
         static void Main(string[] args)   
         {
-            int width, height, x, y;
+            int x, y;
+            double width, height;
             bool s = true;     
             Console.WriteLine("Введите координаты:");
             Console.Write("x="); x = Convert.ToInt32(Console.ReadLine());
             Console.Write("y="); y = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ширина:"); width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Высота:"); height = Convert.ToInt32(Console.ReadLine());
-            NonNegativeInteger(ref width, ref height);
+            Console.WriteLine("Ширина:"); width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Высота:"); height = Convert.ToDouble(Console.ReadLine());
+            NonNegativeNumber(ref width, ref height);
             Console.WriteLine("ширина {0};высота {1}",width, height);
             Rectangles rec = new Rectangles(width, height, x, y);
             while (s)
@@ -51,10 +53,40 @@ namespace lab1
                         rec.GetRec();
                         break;
                     case 2: //размер
-                        Console.WriteLine("Введите новую ширину:"); width = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Введите новую высота:"); height = Convert.ToInt32(Console.ReadLine());
-                        NonNegativeInteger(ref width, ref height);
-                        rec.Resize(width, height);
+                        Console.WriteLine("1. Увеличить\n2. Уменьшить");
+                        int  re= Convert.ToInt32(Console.ReadLine());
+                        switch (re)
+                        {
+                            case 1:
+                                Console.WriteLine("Введите во сколько раз увеличить прямоугольник:");
+                                while (true)
+                                {
+                                    double inc = Convert.ToDouble(Console.ReadLine());
+                                    if (inc<0){
+                                        continue;
+                                    }
+                                    rec.Resize(width, height, inc);
+                                    break;
+                                }
+                                break;
+                            case 2:
+                                Console.WriteLine("Введите во сколько раз уменьшить прямоугольник:");
+                                while (true)
+                                {
+                                    double red = Convert.ToDouble(Console.ReadLine());
+                                    if (red < 0)
+                                    {
+                                        continue;
+                                    }
+                                    red = 1 / red;
+                                    rec.Resize(width, height, red);
+                                    break;
+                                }
+                                break;
+                            default:
+                                break;
+
+                        }
                         rec.GetRec();
                         break;
                     case 3://2 в 1
@@ -64,7 +96,7 @@ namespace lab1
                         Console.WriteLine("Ширина:");  width = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("Высота:");  height = Convert.ToInt32(Console.ReadLine());
                         Rectangles rec2 = new Rectangles(width, height, x, y);
-                        rec.MinRec(rec.width, rec.height, rec.x, rec.y, rec2.width, rec2.height, rec2.x, rec2.y);
+                        rec.MinRec(rec.Width, rec.Height, rec.X, rec.Y, rec2.Width, rec2.Height, rec2.X, rec2.Y);
                         break;
                     case 4: //пересечение 
                         Console.WriteLine("Введите координаты второго прямоугольника:");
@@ -73,7 +105,7 @@ namespace lab1
                         Console.WriteLine("Ширина:"); width = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("Высота:"); height = Convert.ToInt32(Console.ReadLine());
                         Rectangles rec3 = new Rectangles(width, height, x, y);
-                        rec3.Intersection(rec.width, rec.height, rec.x, rec.y, rec3.width, rec3.height, rec3.x, rec3.y);
+                        rec3.Intersection(rec.Width, rec.Height, rec.X, rec.Y, rec3.Width, rec3.Height, rec3.X, rec3.Y);
                         rec3.GetRec();
                         break;
                     default:
